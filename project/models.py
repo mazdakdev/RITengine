664ebb4749a8eba39c0dff205254e9b9acf2eaac
Mazdak <me@mazdak.dev>
2024-07-31 19:49:01 +0330

    Remove null=True from Project.messages

--- project/models.py
+++ project/models.py
@@ -7,4 +7,4 @@ class Project(ShareableModel):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='assets/projects/', blank=True, null=True)
-    messages = models.ManyToManyField(Message, related_name='projects', blank=True, null=True)
+    messages = models.ManyToManyField(Message, related_name='projects', blank=True)
