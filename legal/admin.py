833bcef816931c18b76b01331a52a99d341ab31a
Mazdak <me@mazdak.dev>
2024-08-11 17:46:33 +0330

    Update admin.py
    * Add FAQ

--- legal/admin.py
+++ legal/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import LegalDocument
+from .models import LegalDocument, FaqDocument
 
 admin.site.register(LegalDocument)
+admin.site.register(FaqDocument)
