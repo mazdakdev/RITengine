c2c2ab7d7b6daabc34e04dc138a8595a89de1870
Mazdak <me@mazdak.dev>
2024-08-28 18:59:19 +0330

    feat(stats): add performance over time graph for engines

--- src/engine/admin.py
+++ src/engine/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Engine, Message, Chat, EngineCategory, Assist
 from django.db.models import Count, Case, When, IntegerField
 from stats.models import Vote
-from stats.utils import get_engine_performance
+from stats.utils import get_engine_performance, get_engine_performance_over_time
 
 @admin.register(Engine)
 class EngineAdmin(admin.ModelAdmin):
@@ -10,8 +10,13 @@ class EngineAdmin(admin.ModelAdmin):
 
     def changelist_view(self, request, extra_context=None):
         chart_data = get_engine_performance()
+        popularity_data = get_engine_performance_over_time()
+
         extra_context = extra_context or {}
         extra_context['chart_data'] = list(chart_data)
+        extra_context['popularity_data'] = list(popularity_data)
+
+        print(popularity_data)
 
         return super().changelist_view(request, extra_context=extra_context)
 
--- src/stats/utils.py
+++ src/stats/utils.py
@@ -1,6 +1,10 @@
 from django.db.models import Count, Case, When, IntegerField
-from .models import Vote
 from engine.models import Engine, Message
+from django.db.models.functions import TruncDay
+from .models import Vote
+from django.core.serializers.json import DjangoJSONEncoder
+import json
+from django.utils.dateformat import DateFormat
 
 def get_engine_performance():
     return Engine.objects.annotate(
@@ -14,3 +18,26 @@ def get_engine_performance():
             output_field=IntegerField()
         ))
     ).values('name', 'total_likes', 'total_dislikes')
+
+def get_engine_performance_over_time():
+    data = Engine.objects.annotate(
+        date=TruncDay('messages__votes__created_at'),
+        likes=Count(Case(
+            When(messages__votes__vote_type=Vote.LIKE, then=1),
+            output_field=IntegerField()
+        )),
+        dislikes=Count(Case(
+            When(messages__votes__vote_type=Vote.DISLIKE, then=1),
+            output_field=IntegerField()
+        ))
+    ).values('name', 'date', 'likes', 'dislikes').order_by('date')
+
+    # Convert queryset to a list of dictionaries and handle None or datetime values
+    performance_data = list(data)
+    for entry in performance_data:
+        if entry['date'] is None:
+            entry['date'] = ''
+        else:
+            entry['date'] = DateFormat(entry['date']).format('Y-m-d')
+
+    return performance_data
