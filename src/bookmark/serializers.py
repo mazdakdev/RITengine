b3675a2b038388cf7689464466744c3bb4fc66aa
Mazdak <me@mazdak.dev>
2024-08-31 13:32:57 +0330

    feat(bookmark): group nested msg objects by date

--- src/bookmark/serializers.py
+++ src/bookmark/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from engine.models import Message
 from .models import Bookmark
 from engine.serializers import MessageSerializer
+from collections import defaultdict
+from datetime import datetime
 
 class BookmarkSerializer(serializers.ModelSerializer):
     messages = MessageSerializer(many=True, read_only=True)
@@ -21,3 +23,17 @@ class BookmarkSerializer(serializers.ModelSerializer):
     #     user = self.context.get('user')
     #     bookmark = Bookmark.objects.create(user=user, message=message)
     #     return bookmark
+    #
+    def get_messages(self, obj):
+        grouped_messages = defaultdict(list)
+
+        for message in obj.messages.all():
+            message_date = message.timestamp.strftime('%Y-%m-%d')
+            grouped_messages[message_date].append(MessageSerializer(message).data)
+
+        return grouped_messages
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['messages'] = self.get_messages(instance)
+        return representation
