baef988008c7fbda6593f37766f0bb0d1fdaf04a
Mazdak <me@mazdak.dev>
2024-09-20 20:41:55 +0330

    Revert "Payment"

--- src/payment/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CheckoutSessionView, StripeWebhookView, CustomerPortalView
-
-urlpatterns = [
-    path('checkout/session/', CheckoutSessionView.as_view(), name='checkout'),
-    path('checkout/webhook/', StripeWebhookView.as_view(), name='stripe_webhook'),
-    path('portal/', CustomerPortalView.as_view(), name='stripe_customer_portal'),
-]
